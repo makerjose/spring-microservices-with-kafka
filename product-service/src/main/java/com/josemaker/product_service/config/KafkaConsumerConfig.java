0391ad253a6e1650a6a7bdd890efc104a8456ffe
makerjose <josemakerdeng@gmail.com>
2024-12-15 21:52:59 +0200

    fixes

--- product-service/src/main/java/com/josemaker/product_service/config/KafkaConsumerConfig.java
+++ product-service/src/main/java/com/josemaker/product_service/config/KafkaConsumerConfig.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
@@ -12,24 +13,13 @@ import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import java.util.HashMap;
 import java.util.Map;
 
+@Configuration
 public class KafkaConsumerConfig {
-    private final String productCreatedTopicName;
     private final String bootstrapServers;
-    private final int partitions;
-    private final short replicas;
 
     // constructor dependency injection
-    // partitions and replicas will default to 1 if not defined
-    public KafkaConsumerConfig(
-            @Value("${kafka.topics.product-created.name}") String productCreatedTopicName,
-            @Value("${spring.kafka.bootstrap-servers}") String bootstrapServers,
-            @Value("${kafka.topics.product-created.partitions:3}") int partitions,
-            @Value("${kafka.topics.product-created.replicas:1}") short replicas) {
-
-        this.productCreatedTopicName = productCreatedTopicName;
+    public KafkaConsumerConfig(@Value("${spring.kafka.bootstrap-servers}") String bootstrapServers) {
         this.bootstrapServers = bootstrapServers;
-        this.partitions = partitions;
-        this.replicas = replicas;
     }
 
     @Bean
@@ -40,11 +30,6 @@ public class KafkaConsumerConfig {
         configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return new DefaultKafkaConsumerFactory<>(configProps);
-    }
-
-    @Bean
-    public KafkaTemplate<String, String> kafkaTemplate(ConsumerFactory<String, String> consumerFactory) {
-        return new KafkaTemplate<>(consumerFactory);
     }
 
     public KafkaListenerContainerFactory<
