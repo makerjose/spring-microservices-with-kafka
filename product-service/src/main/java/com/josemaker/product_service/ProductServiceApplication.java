5484f91a413c71a86635bc3d8913167eea1386b3
makerjose <josemakerdeng@gmail.com>
2024-12-15 16:50:32 +0200

    fixes

--- product-service/src/main/java/com/josemaker/product_service/ProductServiceApplication.java
+++ product-service/src/main/java/com/josemaker/product_service/ProductServiceApplication.java
@@ -9,18 +9,8 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @SpringBootApplication(scanBasePackages = {"com.josemaker.product_service"})
 public class ProductServiceApplication {
 
-//	@Value("${kafka.topics.product-created.name}")
-//	private String productCreatedTopic;
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
 
-	// test message
-//	@Bean
-//	CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
-//		return args -> {
-//			kafkaTemplate.send(productCreatedTopic, "hello kafka test");
-//		};
-//	}
 }
